393c515b75211fb00b14605cd36bd95f6097beea
Ed Venator <evenator@gmail.com>
2017-04-02 10:19:09 -0500

    Implement RMSE method.

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include "tools.h"
 
+using Eigen::ArrayXd;
 using Eigen::VectorXd;
-using Eigen::MatrixXd;
 using std::vector;
 
 VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  assert(estimations.size() == ground_truth.size());
+  assert(estimations.size() != 0);
+  ArrayXd sum_err2 = ArrayXd::Zero(estimations.front().size());
+  for (size_t i = 0; i < estimations.size(); ++i) {
+    assert(estimations[i].size() == ground_truth[i].size());
+    ArrayXd err = (estimations[i] - ground_truth[i]).array();
+    sum_err2 += err * err;
+  }
+  return (sum_err2 / estimations.size()).sqrt().matrix();
 }
